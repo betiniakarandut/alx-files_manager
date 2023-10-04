98122d59967cdcec4befdd91cf3e1c434acd4656
betiniakarandut <betiniakarandut@gmail.com>
2023-10-04 03:31:48 +0100

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import { createClient } from "redis";
+
+class RedisClient {
+   constructor(){
+        this.client = createClient();
+        this.client.on('error', (err) => {
+            console.log('Redis client error:', err);
+        })
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err){
+                    reject(err);
+                }
+                else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
+
